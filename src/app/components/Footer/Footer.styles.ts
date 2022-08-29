04db0add7c442d9475cfdfdfd5a9036e50ab057e
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-29 19:44:52 -0300

    Display Flex Footer

--- src/app/components/Footer/Footer.styles.ts
+++ src/app/components/Footer/Footer.styles.ts
@@ -10,3 +10,57 @@ export const Wrapper = styled.footer`
     height: 224px;
   }
 `;
+
+export const Container = styled.div`
+  width: 100%;
+  
+  max-width: 1240px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 28px 16px 8px 16px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (min-width: 992px) {
+    padding: 24px 32px;
+
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+
+export const Info = styled.div`
+  width: 100%;
+  height: 160px;
+
+  @media (min-width: 992px) {
+    width: 296px;
+  }
+
+  background-color: red;
+`;
+
+export const Contact = styled.div`
+  width: 200px;
+  height: 100px;
+
+  background-color: orange;
+`;
+
+export const Realization = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  width: 100%;
+  height: 48px;
+
+  @media (min-width: 992px) {
+    width: 296px;
+    justify-content: flex-end;
+  }
+
+  background-color: green;
+`;
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,7 +3,17 @@ import * as F from "./Footer.styles";
 export default function Footer() {
   return (
     <F.Wrapper>
-      <h1>Footer</h1>
+      <F.Container>
+        <F.Info>
+          <span>Info</span>
+        </F.Info>
+        <F.Contact>
+          <span>Contact</span>
+        </F.Contact>
+        <F.Realization>
+          <span>Realização</span>
+        </F.Realization>
+      </F.Container>
     </F.Wrapper>
   );
 }
