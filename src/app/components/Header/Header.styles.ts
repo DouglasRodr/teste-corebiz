4b21df38c722c92fa8062df33c850f90e337f285
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-29 19:20:15 -0300

    Display Grid Header

--- src/app/components/Header/Header.styles.ts
+++ src/app/components/Header/Header.styles.ts
@@ -2,5 +2,91 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   height: 80px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
   border-bottom: 1px solid red;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1240px;
+  margin: 0 auto;
+  padding: 8px 16px;
+
+  display: grid;
+  grid-template-columns: 25% 1fr 25%;
+  grid-auto-rows: 28px;
+  grid-gap: 4px;
+  grid-template-areas:
+    " column-1 column-2 column-3 "
+    " column-4 column-4 column-4";
+
+  @media (min-width: 1024px) {
+    grid-auto-rows: 36px;
+    grid-gap: 24px;
+    grid-template-columns: 302px 1fr 184px;
+    grid-template-areas: "column-1 column-2 column-3";
+  }
+`;
+
+export const MenuButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+
+  grid-area: column-1;
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
+
+  background-color: pink;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  grid-area: column-2;
+
+  @media (min-width: 1024px) {
+    justify-content: flex-start;
+
+    grid-area: column-1;
+  }
+
+  background-color: red;
+`;
+
+export const SearchInput = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  grid-area: column-4;
+
+  @media (min-width: 1024px) {
+    grid-area: column-2;
+  }
+
+  background-color: orange;
+`;
+
+export const AsideContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+
+  grid-area: column-3;
+
+  background-color: green;
 `;
--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,7 +3,20 @@ import * as H from "./Header.styles";
 export default function Header() {
   return (
     <H.Wrapper>
-      <h1>Header</h1>
+      <H.Container>
+        <H.MenuButton>
+          <span>MenuButton</span>
+        </H.MenuButton>
+        <H.Logo>
+          <span>Logo</span>
+        </H.Logo>
+        <H.SearchInput>
+          <span>Search</span>
+        </H.SearchInput>
+        <H.AsideContent>
+          <span>Aside</span>
+        </H.AsideContent>
+      </H.Container>
     </H.Wrapper>
   );
 }
