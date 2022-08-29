dc7e6ccc38241fb0b1b4a813193fcc3824179720
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-29 18:39:09 -0300

    Base Header e Footer

--- src/app/layout/Default.layout.tsx
+++ src/app/layout/Default.layout.tsx
@@ -1,9 +1,17 @@
 import { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export interface DefaultLayoutProps {
   children?: React.ReactNode;
 }
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
-  return <Fragment>{children}</Fragment>;
+  return (
+    <Fragment>
+      <Header />
+      {children}
+      <Footer />
+    </Fragment>
+  );
 }
