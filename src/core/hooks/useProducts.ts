95cd0b8f343e3b342af33bcdfed987ecdcc0e165
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-30 02:22:04 -0300

    Hook useProducts

---
+++ src/core/hooks/useProducts.ts
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+import { ProductService } from "../../service";
+import { Product } from "../../service/@types";
+
+export default function useProducts() {
+  const [loading, setLoading] = useState(false);
+
+  const [products, setProducts] = useState<Product.Summary[]>([]);
+
+  const fetchProducts = useCallback(async function () {
+    setLoading(true);
+    ProductService.getAllProducts()
+      .then(setProducts)
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    fetchProducts,
+    loading,
+    products,
+  };
+}
