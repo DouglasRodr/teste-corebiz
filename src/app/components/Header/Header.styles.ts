fa19ea27db0cc8156fcae44aae44acc5205e928b
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-29 22:51:23 -0300

    Removendo Borda Header

--- src/app/components/Header/Header.styles.ts
+++ src/app/components/Header/Header.styles.ts
@@ -9,8 +9,6 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  border-bottom: 1px solid red;
 `;
 
 export const Container = styled.div`
